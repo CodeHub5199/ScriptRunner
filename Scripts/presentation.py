ebb522737968e4328d2a6635ee52cb1fe5f9ebd1
Atharv Bhasme <94709794+CodeHub5199@users.noreply.github.com>
2023-07-11 14:38:38 +0530

    Update presentation.py

--- Scripts/presentation.py
+++ Scripts/presentation.py
@@ -1,23 +1,25 @@
 input_req = {'Upload Template':'file_upload', 'Upload Images':'file_upload'}
 
 import argparse
+import sys
 from pptx import Presentation
 from pptx.util import Inches
 
 # Create an argument parser
-parser = argparse.ArgumentParser(description='Create presentation.')
+# parser = argparse.ArgumentParser(description='Create presentation.')
 
 # Add command-line arguments
-parser.add_argument('arg1', nargs='+')
+# parser.add_argument('arg1', nargs='+')
 
 # Parse the command-line arguments
-args = parser.parse_args()
+# args = parser.parse_args()
 
-temp_path = args.arg1
+temp_path = sys.argv[1:]
 
+# print('main_temp_path: ', temp_path)
 template = temp_path[0]
 img_list = temp_path[1:]
-
+#
 # print('temp_path: ', template)
 # print('img_list: ', img_list)
 
@@ -29,26 +31,21 @@ height = Inches(6)
 # template = st.file_uploader('Upload template')
 # photos = st.file_uploader('Upload photos', accept_multiple_files=True)
 
+# def generate_ppt(temp_path):
+
+# print('template: ', template)
 def generate_ppt(temp_path):
-    template = temp_path[0]
-    img_list = temp_path[1:]
     presentation = Presentation(template)
     for filename in img_list:
         # if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
-            # Create a new slide
+        # Create a new slide
         slide = presentation.slides.add_slide(presentation.slide_layouts[6])
         # Add the image to the slide
         picture = slide.shapes.add_picture(filename, left, top, width, height)
-    print('PPT Generated')
-    return presentation
+    presentation.save('modified_template.pptx')
+
+    return 'PPT Generated'
+
 
 
 print(generate_ppt(temp_path))
-# if st.button("Generate and Download PowerPoint"):
-#     generate_ppt(temp_path, img_list)
-#     st.download_button(
-#         label="Download PowerPoint",
-#         data="modified_template.pptx",
-#         file_name="modified_template.pptx",
-#         mime="application/vnd.openxmlformats-officedocument.presentationml.presentation"
-#     )
