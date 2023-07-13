99fb5545a695cc2892f79efc82f28b0c52d1a300
Atharv Bhasme <94709794+CodeHub5199@users.noreply.github.com>
2023-07-13 13:11:54 +0530

    Add files via upload

---
+++ Scripts/subtraction.py
@@ -0,0 +1,24 @@
+input_req = {'Enter first number':'text', 'Enter second number':'text'}
+
+import argparse
+
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Perform addition of two numbers.')
+
+# Add command-line arguments
+parser.add_argument('arg1', type=int, help='First number')
+parser.add_argument('arg2', type=int, help='Second number')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+input1 = args.arg1
+input2 = args.arg2
+
+def sub(input1, input2):
+    result = input1-input2
+    return f'The subtraction of {input1} and {input2} is {result}'
+
+print(sub(input1, input2))
+
+
